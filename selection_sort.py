5028e72f923c632052d81185fda86e9756ca0e5b
Daniel Heffner <cdanielheffner@gmail.com>
2016-08-02 10:27:26 -0500

    prints # of times through the for loop

--- selection_sort.py
+++ selection_sort.py
@@ -1,18 +1,20 @@
 import random
 
 x = []
-for i in range(100):
+for i in range(10000):
 	x.append(int(round(random.random()*10000)))
 
 def selectSort(lst):
 	srted = 0
+	count = 0
 	while srted<len(lst):
 		small = srted
 		for i in range(srted+1,len(lst)):
+			count += 1
 			if lst[i]<lst[small]:
 				small = i
 		lst[srted], lst[small] = lst[small], lst[srted]
 		srted+=1
-	print lst
+	print count
 
 selectSort(x)
